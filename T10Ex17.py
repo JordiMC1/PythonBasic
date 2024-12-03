1e6740553686ed437237a5373dbf99a318b3849c
JordiMC1 <jmelia20941@iesjoanramis.org>
2024-12-03 09:10:55 +0100

    Delete T10Ex17.py

--- T10Ex17.py
+++
@@ -1,9 +0,0 @@
-def esvocal(x):
-    return x.lower() in "aeiou"
-a = "a"
-while(a!="."):
-    a = input("Escriu la vocal: ")
-    if esvocal(a):
-        print("La lletra introduïda {} és vocal".format(a))
-    else:
-        print("La lletra introduïda {} no és una vocal".format(a))
